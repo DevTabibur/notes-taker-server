616c8a53bd068c3edbf6f99b01dc6163c3fd2f65
DevTabibur <tobiburrohman2@gmail.com>
2022-04-28 10:09:08 +0600

    env file is working

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://note-taker:q1iM7wJWgzOqJ2tO@cluster0.hc4xz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hc4xz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
